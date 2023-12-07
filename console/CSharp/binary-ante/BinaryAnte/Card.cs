edccc183d416917e99ae3c32496c7e3bb25c047a
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2023-12-07 22:21:02 +0100

    created class Card, Identifier, Rule, Table ; updated class Deck

---
+++ console/CSharp/binary-ante/BinaryAnte/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using VT.Types.Binary;
+
+namespace VT.BinaryAnte
+{
+	public class Card
+	{
+		private long Id;
+		private Binary Value;
+
+
+		public Card(BinaryType type){
+			Id = new Identifier().NextId();
+			Value = new Binary(type);
+		}
+		public Card(BinaryType type, Identifier identifier){
+			Id = identifier.NextId();
+			Value = new Binary(type);
+		}
+
+
+		long GetId(){
+			return Id;
+		}
+
+
+		public string ToString(){
+			return Id + ":" + Value.ToString();
+		}
+	}
+}
--- console/CSharp/binary-ante/BinaryAnte/Deck.cs
+++ console/CSharp/binary-ante/BinaryAnte/Deck.cs
@@ -5,10 +5,55 @@ namespace VT.BinaryAnte
 {
 	public class Deck
 	{
-		private int id;
+		private int Id;
+		private Card[] deck;
 
 
-		public Deck (BinaryType type){
+		public Deck(int deckSize, Identifier identifier){
+			Id = identifier.NextId(true);
+			deck = new Card[deckSize];
+			//for(int i = 0; i < deckSize; i++) deck[i] = new Card();
+		}
+		public Deck(int deckSize, BinaryType cardType, Identifier identifier){
+			Id = identifier.NextId(true);
+			deck = new Card[deckSize];
+			for(int i = 0; i < deckSize; i++) deck[i] = new Card(cardType, identifier);
+		}
+
+
+		public int GetSize(){
+			return deck.Length;
+		}
+
+		public Card GetTop(){
+			return deck[0];
+		}
+		public Card Get(int position){
+			if (position < deck.Length)
+				return deck[position];
+			else
+				return null;
+		}
+
+
+		public void Toss(){
+			for (int i = 0; i < deck.Length; i += 2) {
+				Random r = new Random();
+				int pos1 = r.Next() % (deck.Length - 1), pos2 = r.Next() % (deck.Length - 1);
+				Card holder = deck[pos1];
+				deck[pos1] = deck[pos2];
+				deck[pos2] = holder;
+			}
+		}
+
+		public void Print(){
+			Console.Write("Deck " + Id + ": ");
+			for(int i = 0; i < deck.Length; i++) Console.Write(deck[i].ToString() + " ");
+			Console.WriteLine("\tTop: " + deck[0].ToString());
+		}
+
+		public void Clear(){
+			deck = null;
 		}
 	}
 }
