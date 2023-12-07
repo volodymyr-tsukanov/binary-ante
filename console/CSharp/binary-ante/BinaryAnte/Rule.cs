f67f76f23d8dc413897f2a263373a6340f7d500d
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2023-12-07 22:42:52 +0100

    Console CSharp v2:  * created class Card, Identifier, Rule, Table ; updated class Deck  *  begin v2 ; reorganized project ; updated class Binary, Deck

---
+++ console/CSharp/binary-ante/BinaryAnte/Rule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VT.BinaryAnte
+{
+	public enum Rules { NOT_SET = 0, DEFAULT = 66 };
+
+
+	public class Rule
+	{
+		private int Code;
+	}
+}
--- console/CSharp/binary-ante/Program.cs
+++ console/CSharp/binary-ante/Program.cs
@@ -1,25 +1,25 @@
 ﻿using System;
+using VT.BinaryAnte;
 using VT.Types.Binary;
 
-namespace VT.BinaryAnte
+class MainClass
 {
-	class MainClass
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
-		{
-			Binary bin = new Binary(BinaryType.bin4, 1);
-			Binary bin2 = new Binary(BinaryType.bin8, 128);
-			Binary bins = bin + bin2;
-			Binary binm = bin2 - bin;
-			Binary bina = bins * bin2;
-			Binary bind = bins / bin2;
+		Binary bin = new Binary(BinaryType.bin4, 1);
+		Binary bin2 = new Binary(BinaryType.bin8, 128);
+		Binary bin3 = new Binary("0010");
+		Binary bins = bin + bin2;
+		Binary binm = bin2 - bin;
+		Binary bina = bins * bin2;
+		Binary bind = bins / bin2;
 
-			Console.WriteLine(bin.ToDec().ToString() + " = " + bin.ToString());
-			Console.WriteLine(bin2.ToDec().ToString() + " = " + bin2.ToString());
-			Console.WriteLine(bins.ToDec().ToString() + " = " + bins.ToString());
-			Console.WriteLine(binm.ToDec().ToString() + " = " + binm.ToString());
-			Console.WriteLine(bina.ToDec().ToString() + " = " + bina.ToString());
-			Console.WriteLine(bind.ToDec().ToString() + " = " + bind.ToString());
-		}
+		Console.WriteLine(bin.ToDec().ToString() + " = " + bin.ToString());
+		Console.WriteLine(bin2.ToDec().ToString() + " = " + bin2.ToString());
+		Console.WriteLine(bin3.ToDec().ToString() + " = " + bin3.ToString());
+		Console.WriteLine(bins.ToDec().ToString() + " = " + bins.ToString());
+		Console.WriteLine(binm.ToDec().ToString() + " = " + binm.ToString());
+		Console.WriteLine(bina.ToDec().ToString() + " = " + bina.ToString());
+		Console.WriteLine(bind.ToDec().ToString() + " = " + bind.ToString());
 	}
 }
